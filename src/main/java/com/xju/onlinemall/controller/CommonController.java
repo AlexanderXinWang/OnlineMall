86ede03e022696c05422259a3b51c35430ddad9e
AlexanderWang <alexander.xin.wang@outlook.com>
2020-07-13 15:20:56 +0800

    规范templates分类方式

--- src/main/java/com/xju/onlinemall/controller/CommonController.java
+++ src/main/java/com/xju/onlinemall/controller/CommonController.java
@@ -3,9 +3,7 @@ package com.xju.onlinemall.controller;
 import com.xju.onlinemall.common.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
@@ -19,7 +17,7 @@ public class CommonController {
         User user =(User) session.getAttribute("user");
         if (user==null){
             model.addAttribute("uname","未登录");
-            return "my-account";
+            return "views_front/my-account";
         }
         else {
             String userName = user.getUserName();
