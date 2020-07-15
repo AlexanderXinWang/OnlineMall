49955c2ab137c9432553511235165e23795052b3
AlexanderWang <alexander.xin.wang@outlook.com>
2020-07-15 23:51:26 +0800

    修复service层bug

--- src/main/java/com/xju/onlinemall/service/StarService.java
+++ src/main/java/com/xju/onlinemall/service/StarService.java
@@ -1,13 +1,5 @@
 package com.xju.onlinemall.service;
 
-import com.github.pagehelper.PageInfo;
-import com.xju.onlinemall.common.domain.Product;
-import com.xju.onlinemall.common.domain.Star;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 public interface StarService {
-    public PageInfo<Product> findStars(int pageNo, int pageSize, Integer userId);
-    public void deleteStarById(Star star);
-    void deleteStarsByIds(List<Integer> ids);
+    String findStars(int pageNo, int pageSize, Integer userId);
 }
